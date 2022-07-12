384e176a71baf462a354ca6fc3650240e8a23259
Villanueva-Flavio <vilanuevaflavioT@gmail.com>
2022-07-12 15:51:49 -0300

    Edit

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-($die)? die():$die = false; // Security for who wants to enter this file directly
-$rconHost = "localhost"; //ip of the RCON Server
-$rconPort = 25575; // Port of the RCON Server
-$rconPassword = ""; //Password of the RCON Server (All config in the server.properties)
-?>
